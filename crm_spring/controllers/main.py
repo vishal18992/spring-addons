0dd331c4641ff256ae118a04411aa9214e05c716
Vishal Patel <vishal.patel18992@gmail.com>
2023-08-29 13:18:02 -0500

    [ADD]crm_spring:Added the lead web api to create lead.

--- crm_spring/controllers/main.py
+++ crm_spring/controllers/main.py
@@ -1,25 +1,37 @@
+import logging
 from odoo import http
-from odoo.addons.phone_validation.tools import phone_validation
 from odoo.http import request
 
-CRM_FIELDS = ['name', 'phone', 'customer', 'salesperson']
+_logger = logging.getLogger(__name__)
+
+CRM_FIELDS = ['name', 'phone', "user_id", "partner_id"]
 
 
 class CrmLeadController(http.Controller):
 
     @http.route("/lead", type='json', auth="public", methods=['POST'], csrf=False)
     def crm_lead_create(self, **kwrgs):
-        print("kwrgs>>>>>>>>>>>>>>>>>>>>>>>>>", kwrgs)
         record = dict({})
+        partner_obj = request.env['res.partner']
+        lead = request.env['crm.lead']
+
         [ record.update({val: kwrgs.get(val, '')}) for val in CRM_FIELDS]
-        print("record", record)
-        # fmt_number = phone_validation.phone_format(
-        #     number, contact_country.code if contact_country else None,
-        #     contact_country.phone_code if contact_country else None,
-        #     force_format='INTERNATIONAL',
-        #     raise_exception=False
-        # )
-        # request.params.update({phone_field: fmt_number})
         
-        return {}
+        customer = partner_obj.search([('name', '=', kwrgs.get('customer'))])
+        sales_person = request.env['res.users'].search(['|', ('name', '=', kwrgs.get('salesperson')), ('login', '=', kwrgs.get('salesperson'))])
+        
+        if not customer:
+            customer = partner_obj.sudo().create({"name": kwrgs.get('customer')})
+
+        record["user_id"] = sales_person.id if sales_person else False
+        record["partner_id"] = customer and customer.id
+        
+        try:
+            lead = request.env['crm.lead'].sudo().create(record)
+        except Exception as ex:
+             _logger.exception('Error while creating lead!!.', ex)
+        
+        return {
+            "id": lead.id
+        }
 
